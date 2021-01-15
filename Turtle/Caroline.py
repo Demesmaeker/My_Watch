7c40ef338a9d260fc93dd28c4ef577614924de40
Demesmaeker <morgane.demesmaeker@gmail.com>
2021-01-15 17:12:14 +0100

    kahoot

--- Turtle/Caroline.py
+++ Turtle/Caroline.py
@@ -13,25 +13,22 @@ caroline.left(90)
 caroline.forward(100)
 caroline.right(90)
 
-# caroline can change
+# # caroline can change
 caroline.speed(3)
 caroline.color('green')
 
-# Caroline can stop drawing
+# # Caroline can stop drawing
 caroline.penup()
 caroline.backward(100)
 caroline.right(90)
 caroline.pendown()
 caroline.forward(100)
 
-
-
-# Caroline can have a friend
+# # Caroline can have a friend
 atome = turtle.Turtle()
 atome.color('blue')
 atome.pensize(20)
 atome.backward(100)
-
 
 
 turtle.done()
